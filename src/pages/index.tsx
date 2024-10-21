fb11758488e85df67af22894ffb84c695a470260
Vinicius <viniciusrob2006@gmail.com>
2024-10-21 16:57:07 -0300

    Terminando de fazer o input de busca

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,14 +26,23 @@ export default function Home() {
   return (
     <div className="relative">
       <Loading identf='Loader' />
-      <div className="w-4/5 border border-black h-[600px]  mx-auto mt-[10vh]">
-        <div className="bg-cyan-600 w-full h-16 flex items-center">
-          <Image src={cnpjimg} alt="foto buscarcnpj" width={dimcnpj} height={dimcnpj} />
-          <p className="ml-6 font-medium text-3xl text-white">Busca Cnpj</p>
+      <div className="w-4/5 border border-black h-[600px] flex flex-col  items-center  mx-auto mt-[10vh]">
+        <div className=" bg-cyan-600 w-full h-16 flex justify-around items-center">
+          <div className="flex items-center ml-6">
+            <Image  src={cnpjimg} alt="foto buscarcnpj" width={dimcnpj} height={dimcnpj} />
+            <p className="ml-4 font-medium text-3xl text-white">Busca Cnpj</p>
+          </div>
+          <form className="bg-white w-72 h-10 ml-60 ">
+            <input className="h-full" type="text" />
+          </form>
         </div>
 
+        <div className="border border-black w-4/5 h-[530px]">
+          
         </div>
 
-        </div>
+      </div>
+
+    </div>
   );
 }
